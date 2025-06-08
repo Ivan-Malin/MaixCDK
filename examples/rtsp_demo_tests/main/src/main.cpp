a8bcf37bca3abcd8a2635c1b3c0ad8626b2de81f
Ivan-Malin <malili2003li@gmail.com>
2025-06-08 21:55:50 +0300

    Small fix

--- examples/rtsp_demo_tests/main/src/main.cpp
+++ examples/rtsp_demo_tests/main/src/main.cpp
@@ -54,7 +54,7 @@ int _main(int argc, char* argv[])
 
     // Part of overlapping image
     camera::Camera *cam2 = cam.add_channel(320, 240);
-    rtsp::Region *region = rtsp.add_region(0, 0, rgn_img->width(), rgn_img->height());
+    rtsp::Region *region = rtsp.add_region(0, 0, 320, 240);
     
 
     uint64_t last_ms = time::ticks_ms();
