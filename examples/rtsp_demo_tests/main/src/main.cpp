58d76e1f6c33bddd1d857cc50d075f7e92bd46db
Ivan-Malin <malili2003li@gmail.com>
2025-06-09 00:16:25 +0300

    Some small fixes

--- examples/rtsp_demo_tests/main/src/main.cpp
+++ examples/rtsp_demo_tests/main/src/main.cpp
@@ -84,9 +84,10 @@ int _main(int argc, char* argv[])
 
         // Out
         log::info("A1");
-        img->to_format(image::FMT_BGR888);
+        image::Image img_bgr888 = img->to_format(image::FMT_BGR888);
         log::info("A2");
         Bytes* data_out = img->to_bytes(true);
+        delete img_bgr888;
         log::info("A3");
         uint8_t* data_out_raw = data_out->begin();
         log::info("A4");
