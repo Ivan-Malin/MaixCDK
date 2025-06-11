d9c9c0a9cd42f391fb0d598f8af9ed107fffa0ca
Ivan-Malin <malili2003li@gmail.com>
2025-06-11 19:59:37 +0300

    Trying to fix

--- examples/idrone-aruco-detector/main/src/main.cpp
+++ examples/idrone-aruco-detector/main/src/main.cpp
@@ -7,7 +7,7 @@
 #include "maix_rtsp.hpp"
 #include "maix_camera.hpp"
 #include "maix_basic.hpp"
-#include "csignal"
+#include <csignal>
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
@@ -16,86 +16,84 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/freetype.hpp"
 #include "opencv2/aruco.hpp"
-#include <nlohmann/json.hpp>  // или #include <nlohmann/json.hpp>, в зависимости от используемой библиотеки JSON
-#include <iostream>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 using namespace maix;
+using json = nlohmann::json;
 
 class ArucoModule : public ConfigurableSocketModule {
 public:
     ArucoModule(const std::string& controller_ip, const std::string& module_name) :
         ConfigurableSocketModule(controller_ip, module_name)
     {
-        // Здесь можно инициализировать параметры ArUco, если нужно
+        // Используем cv::Ptr для хранения словаря
         dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     }
 
 protected:
     void processor_run() override {
-        // Получаем пакет с изображением
         Packet* packet = get_sub_data_packet("frame");
         if (!packet) {
             std::cerr << "Failed to get frame packet" << std::endl;
             return;
         }
 
-        // Время задержки
         std::cout << "Delay_ns: " << get_uptime_nanoseconds() - packet->emit_time_ns << std::endl;
 
-        // Конвертируем в cv::Mat
-        cv::Mat img_maix = packet_to_cv_mat(packet);
+        cv::Mat img_maix = Packet::packet_to_cv_mat(packet);
 
-        // Результаты детекции
         std::vector<std::vector<cv::Point2f>> corners, rejected;
         std::vector<int> ids;
 
-        // Обнаружение ArUco маркеров
+        // Исправленный вызов detectMarkers с использованием cv::Ptr
         cv::aruco::detectMarkers(img_maix, dictionary, corners, ids, detectorParams, rejected);
 
-        // Подготавливаем JSON-результат
-        Json::Value detected_markers; // или nlohmann::json detected_markers;
+        // Используем nlohmann::json вместо Json::Value
+        json detected_markers;
 
         // Записываем ids
+        detected_markers["ids"] = json::array();
         for (int id : ids) {
-            detected_markers["ids"].append(id);
+            detected_markers["ids"].push_back(id);
         }
 
         // Записываем углы
+        detected_markers["corners"] = json::array();
         for (const auto& corner : corners) {
-            Json::Value corner_array;
+            json corner_array = json::array();
             for (const auto& point : corner) {
-                Json::Value pt;
-                pt.append(point.x);
-                pt.append(point.y);
-                corner_array.append(pt);
+                json pt = json::array();
+                pt.push_back(point.x);
+                pt.push_back(point.y);
+                corner_array.push_back(pt);
             }
-            detected_markers["corners"].append(corner_array);
+            detected_markers["corners"].push_back(corner_array);
         }
 
         // Записываем rejected
+        detected_markers["rejected"] = json::array();
         for (const auto& rej : rejected) {
-            Json::Value rej_array;
+            json rej_array = json::array();
             for (const auto& point : rej) {
-                Json::Value pt;
-                pt.append(point.x);
-                pt.append(point.y);
-                rej_array.append(pt);
+                json pt = json::array();
+                pt.push_back(point.x);
+                pt.push_back(point.y);
+                rej_array.push_back(pt);
             }
-            detected_markers["rejected"].append(rej_array);
+            detected_markers["rejected"].push_back(rej_array);
         }
 
         // Создаём выходной пакет
-        Packet* output_packet = new Packet(detected_markers, get_uptime_nanoseconds());
+        Packet* output_packet = new Packet(detected_markers.dump(), get_uptime_nanoseconds());
         set_pub_data_packet("detected_markers", output_packet);
 
         // Очистка
-        delete output_packet;
         delete packet;
     }
 
 private:
-    cv::aruco::Dictionary dictionary;
+    cv::Ptr<cv::aruco::Dictionary> dictionary;  // Исправлен тип на cv::Ptr
     cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
 };
 
@@ -120,11 +118,6 @@ int _main(int argc, char* argv[])
 
 int main(int argc, char* argv[])
 {
-    // Catch signal and process
     sys::register_default_signal_handle();
-
-    // Use CATCH_EXCEPTION_RUN_RETURN to catch exception,
-    // if we don't catch exception, when program throw exception, the objects will not be destructed.
-    // So we catch exception here to let resources be released(call objects' destructor) before exit.
     CATCH_EXCEPTION_RUN_RETURN(_main, -1, argc, argv);
 }
