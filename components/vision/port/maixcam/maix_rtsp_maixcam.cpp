bc2982af74320e36572dc51585b16afdf6ea3b6b
Ivan-Malin <malili2003li@gmail.com>
2025-06-12 22:19:29 +0300

    Debugging

--- components/vision/port/maixcam/maix_rtsp_maixcam.cpp
+++ components/vision/port/maixcam/maix_rtsp_maixcam.cpp
@@ -453,7 +453,15 @@ namespace maix::rtsp
             delete param->encoder;
             param->encoder = nullptr;
         }
-        std::cout << "RTSP9" << std::endl;
+        std::cout << "RTSP9" \
+            << param->camera->width() << ", " \
+            << param->camera->height() << ", " \
+            << image::Format::FMT_YVU420SP << ", " \
+            << video::VIDEO_H264 << ", " \
+            << param->fps << ", " \
+            << 50 << ", " \
+            << param->encoder_bitrate \
+            << std::endl;
         param->encoder = new video::Encoder("", param->camera->width(), param->camera->height(), image::Format::FMT_YVU420SP, video::VIDEO_H264, param->fps, 50, param->encoder_bitrate);
         std::cout << "RTSP10" << std::endl;
         err::check_null_raise(param->encoder, "Create video encoder failed!");
